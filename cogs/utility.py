eabd2a4cea4bff4f261347d7ed502ee278a55da4
Der-Eddy <dereddyteddy@gmail.com>
2017-09-20 02:14:10 +0200

    :ranks command will now display all roles on the server, also works now on other servers than Coding Lounge

--- cogs/utility.py
+++ cogs/utility.py
@@ -500,31 +500,41 @@ class utility():
 
         :ranks Python
         '''
-        if ctx.message.server.id != '357603732634075136': #Coding Lounge 2.0
-            await self.bot.say(':x: This command only works on the Coding Lounge Server!')
-            return
-
         rankList = ['HTML / CSS', 'Javascript', 'C/C++', '.NET', 'PHP', 'NSFW',
                     'Java', 'Gourmet', 'Assembler', 'Python', 'Math', 'AutoIt']
 
-        if len(rankName) == 0:
-            rolesList = ''
+        if len(rankName) == 0 and ctx.message.server.id != '357603732634075136' or ''.join(rankName) == 'all':
+            rolesList = '`'
+            for roleServer in ctx.message.server.roles:
+                if not roleServer.is_everyone:
+                    count = 0
+                    for member in ctx.message.server.members:
+                        if roleServer in member.roles:
+                            count += 1
+                    rolesList += f'{roleServer.name:30}{count}\n'
+            embed = discord.Embed(color=0xf1c40f) #Golden
+            embed.set_thumbnail(url=ctx.message.server.icon_url)
+            embed.add_field(name='Ranks', value=rolesList + '`', inline=True)
+            await self.bot.say(embed=embed)
+        elif len(rankName) == 0 and ctx.message.server.id == '357603732634075136':
+            rolesList = '`'
             for role in rankList:
                 count = 0
                 roleServer = discord.utils.get(ctx.message.server.roles, name=role)
                 for member in ctx.message.server.members:
                     if roleServer in member.roles:
                         count += 1
-                rolesList += '{:20}{:>10} Member\n'.format(role, count)
-            embed = discord.Embed(color=0xf1c40f) #Golden
+                rolesList += f'{role:20}{count}\n'
+            embed = discord.Embed(color=0x3498db) #Blue
             embed.set_thumbnail(url=ctx.message.server.icon_url)
             embed.set_footer(text='Use the ":rank RANKNAME" command to join a rank')
-            embed.add_field(name='Ranks', value=rolesList, inline=True)
+            embed.add_field(name='Ranks', value=rolesList + '`', inline=True)
             await self.bot.say(embed=embed)
-            return
-        else:
+        elif ctx.message.server.id != '357603732634075136':
+            await self.bot.say(':x: This command only works on the Coding Lounge Server!')
+        elif ctx.message.server.id == '357603732634075136':
             rankName = ' '.join(rankName)
-            #Avoiding some common pitfalls ...
+            #Avoiding some common pitfalls ... and some if checks ...
             rankName = rankName.replace('HTML/CSS', 'HTML / CSS')
             rankName = rankName.replace('javascript', 'Javascript')
             rankName = rankName.replace('js', 'Javascript')
