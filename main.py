9686f9767d341fdd7f34a4db047e4ba70a42b2e0
Eduard Nikoleisen <dereddyteddy@gmail.com>
2019-09-24 17:00:58 +0200

    Bumped version number

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import discord
 from discord.ext import commands
 import loadconfig
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 logger = logging.getLogger('discord')
 #logger.setLevel(logging.DEBUG)
