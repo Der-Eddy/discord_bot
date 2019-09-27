d48caa91a30e947968e14759e584612234a2424d
Yannick <24491035+zaanposni@users.noreply.github.com>
2019-09-27 13:34:01 +0200

    Update main.py
    updated link to bot channel

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ async def on_message(message):
     if message.author.bot or message.author.id in loadconfig.__blacklist__:
         return
     if isinstance(message.channel, discord.DMChannel):
-        await message.author.send(':x: Sorry, but I don\'t accept commands through direct messages! Please use the `#bots` channel of your corresponding server!')
+        await message.author.send(':x: Sorry, but I don\'t accept commands through direct messages! Please use the <#216566439199506432> channel of your corresponding server!')
         return
     if bot.dev and not await bot.is_owner(message.author):
         return
